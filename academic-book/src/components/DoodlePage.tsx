690302010fa5034cb4865da05c67751bc37f6b34
Pratik Pawar <204126908+PratikPawar020403@users.noreply.github.com>
2025-12-23 22:08:33 +0530

    Update DoodlePage.tsx

--- academic-book/src/components/DoodlePage.tsx
+++ academic-book/src/components/DoodlePage.tsx
@@ -2,28 +2,43 @@ import React, { forwardRef } from 'react';
 import Page from './Page';
 
 interface DoodlePageProps {
-    children?: React.ReactNode;
-    style?: React.CSSProperties;
-    className?: string;
-    number?: number;
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+  className?: string;
+  number?: number;
 }
 
 const DoodlePage = forwardRef<HTMLDivElement, DoodlePageProps>((props, ref) => {
-    return (
-        <Page ref={ref} variant="plain" className={`doodle-page ${props.className || ''}`} {...props}>
-            <div className="page-content" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '10px 90px 50px 0px' }}>
-                <img src="/this.jpg"
-                    alt="Doodle Art"
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'fill',
-                        display: 'block'
-                    }}
-                />
-            </div>
-        </Page>
-    );
+  return (
+    <Page
+      ref={ref}
+      variant="plain"
+      className={`doodle-page ${props.className || ''}`}
+      {...props}
+    >
+      <div
+        className="page-content"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+          padding: '40px'
+        }}
+      >
+        <img
+          src="/this.jpg"
+          alt="Doodle Art"
+          style={{
+            maxWidth: '100%',
+            maxHeight: '100%',
+            objectFit: 'contain',
+            display: 'block'
+          }}
+        />
+      </div>
+    </Page>
+  );
 });
 
 export default DoodlePage;
