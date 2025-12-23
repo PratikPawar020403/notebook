a4e24f99b558668fee1870ac06ec888670641b3a
Pratik Pawar <204126908+PratikPawar020403@users.noreply.github.com>
2025-12-23 23:08:11 +0530

    Update DoodlePage.tsx

--- academic-book/src/components/DoodlePage.tsx
+++ academic-book/src/components/DoodlePage.tsx
@@ -2,44 +2,29 @@ import React, { forwardRef } from 'react';
 import Page from './Page';
 
 interface DoodlePageProps {
-  children?: React.ReactNode;
-  style?: React.CSSProperties;
-  className?: string;
-  number?: number;
+    children?: React.ReactNode;
+    style?: React.CSSProperties;
+    className?: string;
+    number?: number;
 }
 
 const DoodlePage = forwardRef<HTMLDivElement, DoodlePageProps>((props, ref) => {
-  return (
-    <Page
-      ref={ref}
-      variant="plain"
-      className={`doodle-page ${props.className || ''}`}
-      {...props}
-    >
-      <div
-        className="page-content"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'flex-start', // 
-          height: '100%',
-          padding: '40px 60px 40px 40px',
-          boxSizing: 'border-box'
-        }}
-      >
-        <img
-          src="/doodle.jpg" // 
-          alt="Doodle Art"
-          style={{
-            width: '100%',
-            maxHeight: '90%',
-            objectFit: 'contain', // 
-            display: 'block'
-          }}
-        />
-      </div>
-    </Page>
-  );
+    return (
+        <Page ref={ref} variant="plain" className={`doodle-page ${props.className || ''}`} {...props}>
+            <div className="page-content" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '10px 90px 50px 0px' }}>
+                <img
+                    src="doodle.jpg"
+                    alt="Doodle Art"
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'fill',
+                        display: 'block'
+                    }}
+                />
+            </div>
+        </Page>
+    );
 });
 
 export default DoodlePage;
